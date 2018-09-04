80d7269c64add18fa62d264a666fbaa9d009ca28
pmahendro <pmahendro@bloomingtech-asus>
2018-09-04 16:58:28 +0700

    Add collection namespace using in entity

--- src/AppBundle/Entity/Transaction/PurchaseInvoiceDownpayment.php
+++ src/AppBundle/Entity/Transaction/PurchaseInvoiceDownpayment.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity\Transaction;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use AppBundle\Entity\Common\CodeNumberEntity;
