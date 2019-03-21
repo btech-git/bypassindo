b2d1cfa750f195609312b49bc217931d5acaf270
pmahendro <pmahendro@bloomingtech-asus>
2019-03-21 11:45:08 +0700

    modify tax nominal to text field

--- src/AppBundle/Form/Transaction/PurchaseInvoiceHeaderUnitType.php
+++ src/AppBundle/Form/Transaction/PurchaseInvoiceHeaderUnitType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Validator\Constraints\Valid;
 use Symfony\Component\Validator\Constraints\Count;
 use LibBundle\Form\Type\EntityTextType;
@@ -25,6 +26,8 @@ class PurchaseInvoiceHeaderUnitType extends AbstractType
             ->add('supplierInvoiceNumber')
             ->add('taxInvoiceDate', 'date')
             ->add('taxInvoiceNumber')
+            ->add('taxNominal', HiddenType::class)
+            ->add('taxNominalReplacement')
             ->add('note')
             ->add('isTax')
             ->add('supplier', EntityTextType::class, array('class' => Supplier::class))
