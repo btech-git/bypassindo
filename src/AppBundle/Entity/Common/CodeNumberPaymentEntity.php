0cc33c92c1715b6899d624bbb9cd445a19e920fc
pmahendro <pmahendro@bloomingtech-asus>
2020-02-26 10:56:42 +0700

    fix code number for deposit and expense

--- src/AppBundle/Entity/Common/CodeNumberPaymentEntity.php
+++ src/AppBundle/Entity/Common/CodeNumberPaymentEntity.php
@@ -8,8 +8,9 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\MappedSuperclass
+ * @UniqueEntity({"codeNumberOrdinal", "codeNumberMonth", "codeNumberYear", "accountCode"})
  */
-abstract class CodeNumberAccountEntity
+abstract class CodeNumberPaymentEntity
 {
     /**
      * @ORM\Column(type="smallint")
@@ -26,10 +27,13 @@ abstract class CodeNumberAccountEntity
      * @Assert\NotNull() @Assert\GreaterThan(0)
      */
     private $codeNumberYear;
-
+    /**
+     * @ORM\Column(type="string", length=20)
+     * @Assert\NotBlank()
+     */
+    private $accountCode;
+    
     abstract public function getCodeNumberConstant();
-
-    abstract public function getCodeNumberAccountCode();
     
     public function getCodeNumberOrdinal() { return $this->codeNumberOrdinal; }
     public function setCodeNumberOrdinal($codeNumberOrdinal) { $this->codeNumberOrdinal = $codeNumberOrdinal; }
@@ -39,39 +43,44 @@ abstract class CodeNumberAccountEntity
     
     public function getCodeNumberYear() { return $this->codeNumberYear; }
     public function setCodeNumberYear($codeNumberYear) { $this->codeNumberYear = $codeNumberYear; }
-
+    
+    public function getAccountCode() { return $this->accountCode; }
+    public function setAccountCode($accountCode) { $this->accountCode = $accountCode; }
+    
     public function getCodeNumber()
     {
         $numerals = self::makeRomanNumerals();
         
-        return sprintf('%04d/%s/%s/%02d/%s', intval($this->codeNumberOrdinal), $this->getCodeNumberConstant(), $numerals[intval($this->codeNumberMonth)], intval($this->codeNumberYear), $this->getCodeNumberAccountCode());
+        return sprintf('%04d/%s/%s/%02d/%s', intval($this->codeNumberOrdinal), $this->getCodeNumberConstant(), $numerals[intval($this->codeNumberMonth)], intval($this->codeNumberYear), $this->accountCode);
     }
     
     public function setCodeNumber($codeNumber)
     {
         $nums = array_flip(self::makeRomanNumerals());
         
-        list($ordinal, , $month, $year, ) = explode('/', $codeNumber);
+        list($ordinal, , $month, $year, $accountCode) = explode('/', $codeNumber);
         
         $this->codeNumberOrdinal = intval($ordinal);
         $this->codeNumberMonth = $nums[$month];
         $this->codeNumberYear = intval($year);
+        $this->accountCode = $accountCode;
     }
     
-    public function setCodeNumberToNext($codeNumber, $currentYear, $currentMonth, $account)
+    public function setCodeNumberToNext($codeNumber, $currentYear, $currentMonth, $accountCode)
     {
         $this->setCodeNumber($codeNumber);
         
         $cnMonth = intval($currentMonth);
         $cnYear = intval($currentYear);
         $ordinal = $this->codeNumberOrdinal;
-        if ($cnMonth > $this->codeNumberMonth || $cnYear > $this->codeNumberYear || $account->getCode() !== $this->getCodeNumberAccountCode()) {
+        if ($cnMonth > $this->codeNumberMonth || $cnYear > $this->codeNumberYear || $accountCode !== $this->accountCode) {
             $ordinal = 0;
         }
         
         $this->codeNumberOrdinal = $ordinal + 1;
         $this->codeNumberMonth = $cnMonth;
         $this->codeNumberYear = $cnYear;
+        $this->accountCode = $accountCode;
     }
     
     private static function makeRomanNumerals()
